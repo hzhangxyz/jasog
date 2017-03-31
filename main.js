753161b8f3ff9153b490e2d30cf259cc37b1ed55
zh970205@outlook.com <zh19970205@126.com>
2017-04-01 03:59:14 +0800

    first-commit

---
+++ main.js
@@ -0,0 +1,46 @@
+var storage = sessionStorage
+
+var clear_data = async () => {
+    storage.clear()
+}
+
+var get_data = async (n) => {
+    if(storage.hasOwnProperty(n)){
+        return storage.getItem(n)
+    }else{
+        var value = await (await fetch(n)).text()
+        storage.setItem(n,value)
+        return value
+    }
+}
+
+var _html = async (n) => {
+    var to_insert = await get_data(`html/${n}.html`)
+    var config = JSON.parse(to_insert.match(/^\s*<!--([\s\S]*?)\s*-->/)[1])
+    if(!config.hasOwnProperty("layout")){
+        return [to_insert,config]
+    }else{
+        var [doc,ans] = await _html(config.layout)
+        for(var i in config){
+            ans[i] = config[i]
+        }
+        return [doc.replace(/<!--\s*[cC][oO][nN][tT][eE][nN][tT]\s*-->/,to_insert),ans]
+    }
+}
+
+var html = async (n) => {
+    [doc, ans] = await _html(n)
+    for(var i in ans){
+        this[i] = ans[i]
+    }
+    document.write(doc)
+}
+
+var json = async (n,m = window) => {
+    data = JSON.parse(await get_data(`json/${n}.json`))
+    console.log(data)
+    for(var i in data){
+        m[i] = data[i]
+    }
+    return data
+}
