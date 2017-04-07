5fcfee302abb5af8f2c95c0dcc396e921956768a
trumpet <zh970205@outlook.com>
2017-04-07 17:56:51 +0800

    beautify

--- main.js
+++ main.js
@@ -13,47 +13,56 @@ var static_router = express.static("jasog")
 
 var main_router = express.Router()
 
-//var include 
-//var from
-//var content
-//var write
-
 main_router.use("/main.js",async function(req,res,next){
     res.send("main.js forbidden\n")
 })
 
 var toSend = function(temp){
+
     for(var i in temp){
-        this[i]=temp[i]
+        this[i] = temp[i]
     }
-    this.data=""
-    this.write=async function(temp){
+
+    this.data = ""
+
+    this.contents = []
+
+    this.point = []
+
+    this.getfile = async function(temp){
+        return (await asyncalize(fs.readFile)(temp))[1].toString()
+    }
+
+    this.write = async function(temp){
         this.data += temp
     }
-    this.eval=async function(temp){
+    this.eval = async function(temp){
         await eval(`(async function(){${temp}})`).call(this)
     }
-   this.add_doc=async function(doc){
+
+    this.add_doc = async function(doc){
         var temp_doc = `?>${doc}<?`
         var htmls=temp_doc.match(/\?>[\s\S]*?<\?/g) || []
         var jss=temp_doc.match(/<\?([\s\S]*?)\?>/g) || []
         this.contents.push({htmls,jss})
         return this.contents.length-1
     }
-    this.content=async function(temp){
+
+    this.content = async function(temp){
         this.point.push(this.point[0])
         this.point.shift()
     }
-    this.include=async function(temp){
-        var doc = (await asyncalize(fs.readFile)(temp))[1].toString()
+
+    this.include = async function(temp){
+        var doc = await this.getfile(temp)
         this.point.unshift(await this.add_doc(doc))
     }
-    this.from=async function(temp){
-        var doc = (await asyncalize(fs.readFile)(temp))[1].toString()
+
+    this.from= async function(temp){
+        var doc = await this.getfile(temp)
         this.point.unshift(await this.add_doc(doc))
     }
-    this.contents=[]
-    this.point=[]
+
     this.render = async function(){
         var now
         var length
@@ -72,9 +81,8 @@ var toSend = function(temp){
 }
  
 main_router.use(/.*\.jsg/,async function(req,res,next){
-    doc = (await asyncalize(fs.readFile)(req.baseUrl.substr(1)))[1].toString()
     var to_send=new toSend()
-    to_send.point.unshift(await to_send.add_doc(doc))
+    to_send.point.unshift(await to_send.add_doc(await to_send.getfile(req.baseUrl.substr(1))))
     await to_send.render()
     res.send(to_send.data)
 })
